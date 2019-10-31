d91148cb628f1f6e43c2bec8064b06dbb36de973
David McCormick <davidmccormick27410@gmail.com>
2019-10-31 13:16:41 -0400

    Added image of Ignaz

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='ignaz-moscheles-1794-1870-granger.jpg' className="App-logo" alt='Ignaz Moscheles'/>
       </header>
     </div>
   );
